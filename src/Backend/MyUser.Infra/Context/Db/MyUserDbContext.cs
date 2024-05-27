c6b7b5023e2af7d743c36829c14b54a06bdc29e5
vts1978 <vinicioussouza@gmail.com>
2024-05-27 17:46:08 -0300

    add: dbcontext

---
+++ src/Backend/MyUser.Infra/Context/Db/MyUserDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyUser.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUser.Infra.Context.Db;
+public class MyUserDbContext : DbContext
+{
+    public MyUserDbContext(DbContextOptions<MyUserDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Addresses)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserId);
+
+    }
+}
